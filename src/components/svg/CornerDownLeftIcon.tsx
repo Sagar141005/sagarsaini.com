75ed2f3713113e40b2a67635fff10712ba09dda3
Sagar141005 <samsaini5566@gmail.com>
2025-12-25 01:56:08 +0530

    feat(icon): add custom svg icons

---
+++ src/components/svg/CornerDownLeftIcon.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function CornerDownLeftIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="10"
+      height="10"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      aria-hidden="true"
+    >
+      <path d="M20 4v7a4 4 0 0 1-4 4H4"></path>
+      <path d="m9 10-5 5 5 5"></path>
+    </svg>
+  );
+}
