01f7a0e79c1408882c76afce829705022fee405e
Sagar141005 <samsaini5566@gmail.com>
2025-12-25 12:48:31 +0530

    fix(icon): use camelCase format

--- src/components/svg/AboutIcon.tsx
+++ src/components/svg/AboutIcon.tsx
@@ -9,9 +9,9 @@ export default function AboutIcon() {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <path d="M15 5h6" />
       <path d="M15 12h6" />
