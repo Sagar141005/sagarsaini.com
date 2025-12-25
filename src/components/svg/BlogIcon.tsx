888839fc384c48bcabc7ab2a1ca0c09ed141cbf8
Sagar141005 <samsaini5566@gmail.com>
2025-12-25 20:56:12 +0530

    fix(icon): use camelCase format

--- src/components/svg/BlogIcon.tsx
+++ src/components/svg/BlogIcon.tsx
@@ -9,9 +9,9 @@ export default function BlogIcon() {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       aria-hidden="true"
     >
       <path d="M4 11a9 9 0 0 1 9 9"></path>
--- src/components/svg/CornerDownLeftIcon.tsx
+++ src/components/svg/CornerDownLeftIcon.tsx
@@ -9,9 +9,9 @@ export default function CornerDownLeftIcon() {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       aria-hidden="true"
     >
       <path d="M20 4v7a4 4 0 0 1-4 4H4"></path>
