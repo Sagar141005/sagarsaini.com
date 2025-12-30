67ffe12a650d70b05fba6b9c8d887d2af1d86bc2
Sagar141005 <samsaini5566@gmail.com>
2025-12-30 12:51:56 +0530

    refactor: structure /projects page with reusable components

--- src/app/(site)/projects/page.tsx
+++ src/app/(site)/projects/page.tsx
@@ -4,24 +4,20 @@ import UpcomingProject from "@/components/project/UpcomingProject";
 import ContentHeader from "@/components/common/ContentHeader";
 import projects from "@/data/projects";
 import { UPCOMING_DATA } from "@/data/upcomingProjects";
+import Container from "@/components/common/Container";
 
 const page = () => {
   return (
-    <div className="w-full max-w-3xl mx-auto px-4 pt-16 bg-white dark:bg-[#09090B]">
-      <section className="mb-16 space-y-6">
-        <ContentHeader
-          heading="Projects"
-          subHeading="A curated collection of projects I’ve worked on — spanning full-stack
-          apps, open-source tools, design experiments, and real-time or
-          AI-integrated builds. These reflect my passion for clean architecture,
-          intuitive UX, and pushing technical boundaries."
-          size="lg"
-        />
-        <ProjectList projects={projects} />
-      </section>
+    <Container className="py-16 space-y-8">
+      <ContentHeader
+        heading="Projects"
+        subHeading="My projects and work across various technologies and domains, exploring innovative solutions and practical applications."
+        size="lg"
+      />
+      <ProjectList projects={projects} showAll />
 
       <UpcomingProject projects={UPCOMING_DATA} />
-    </div>
+    </Container>
   );
 };
 
