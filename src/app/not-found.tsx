63528c8854c79d83dd3aef6fc10e8798ce895a95
Sagar141005 <samsaini5566@gmail.com>
2026-01-01 01:09:06 +0530

    refactor(ui): use reusable button component instead of link tag and add font-mono in heading

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,18 +1,18 @@
+import { Button } from "@/components/ui/button";
 import { ArrowRightIcon } from "lucide-react";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
-      <h1 className="mt-8 mb-6 font-mono text-8xl font-medium">404</h1>
+      <h1 className="mt-8 mb-6 !font-mono text-8xl font-medium">404</h1>
 
-      <Link
-        href="/"
-        className="text-sm font-light bg-black dark:bg-white text-white dark:text-black flex items-center gap-1 py-2 px-4 rounded-full hover:opacity-90 transition"
-      >
-        Go to Home
-        <ArrowRightIcon className="size-4" />
-      </Link>
+      <Button asChild>
+        <Link href="/">
+          Go to Home
+          <ArrowRightIcon className="size-4" />
+        </Link>
+      </Button>
     </div>
   );
 }
