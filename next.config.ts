ac61aa4db8aa513a8408c54f5f16f4cdbc53b61d
Sagar141005 <samsaini5566@gmail.com>
2026-02-11 23:37:52 +0530

    feat: add remote image support for unsplash and dribble

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      // Unsplash
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+
+      // Dribbble (shots + CDN)
+      {
+        protocol: "https",
+        hostname: "cdn.dribbble.com",
+      },
+      {
+        protocol: "https",
+        hostname: "dribbble.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
